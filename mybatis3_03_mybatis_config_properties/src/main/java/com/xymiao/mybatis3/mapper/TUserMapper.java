22fc92d8d31df5ca8cb739ba3e72847a5d781121
xymiao <xymiaotec@126.com>
2019-12-27 14:39:24 +0800

    mybatis全局config配置中properties属性

---
+++ mybatis3_03_mybatis_config_properties/src/main/java/com/xymiao/mybatis3/mapper/TUserMapper.java
@@ -0,0 +1,12 @@
+package com.xymiao.mybatis3.mapper;
+
+import com.xymiao.mybatis3.pojo.TUser;
+
+public interface TUserMapper {
+	/**
+	 * 根据主键查询对应的用户数据, 并且使用实体类接收数据
+	 * @param uid 主键
+	 * @return 用户信息
+	 */
+	TUser selectUser(String uid);
+}
---
+++ mybatis3_03_mybatis_config_properties/src/test/java/com/xymiao/mybatis3/test/UserMapperTest.java
@@ -0,0 +1,40 @@
+package com.xymiao.mybatis3.test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.xymiao.mybatis3.mapper.TUserMapper;
+import com.xymiao.mybatis3.pojo.TUser;
+
+public class UserMapperTest {
+	private SqlSessionFactory sqlSessionFactory  = null;
+	@Before
+	public void init() throws IOException {
+		// 配置文件地址 根据/resources的root查找
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		// 创建一个session工厂.
+		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	}
+	
+	@Test
+	public void test() {
+		// 第二种方式, 推荐使用这种
+		try (SqlSession session = sqlSessionFactory.openSession()) {
+			TUserMapper mapper = session.getMapper(TUserMapper.class);
+			TUser tUser  = mapper.selectUser("1");
+			assertArrayEquals(new Boolean[] {Objects.isNull(tUser)}, new Boolean[] {false});
+		}
+	}
+
+}
